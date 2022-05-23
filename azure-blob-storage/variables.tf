ff274c211eb0828b1926f6bbb344d0bf443ec5f2
Filip Ostrowski <91828926+aod244@users.noreply.github.com>
2022-05-23 14:06:56 +0200

    Update variables.tf

--- azure-blob-storage/variables.tf
+++ azure-blob-storage/variables.tf
@@ -7,3 +7,33 @@ variable "location" {
     type = string
     default = "West Europe"
 }
+
+variable "account_name" {
+    type = string
+    default = "abc123@123mail.com"
+}
+
+variable "acc_tier" {
+    type = string
+    default = "Standard"
+}
+
+variable "acc_rep_type" {
+    type = string
+    default = "GRS"
+}
+
+variable "environment" {
+    type = string
+    default = "staging"
+}
+
+variable "container_name" {
+    type = string
+    default = "cantainer1"
+}
+
+variable "source" {
+    type = string
+    default = "some-file.zip"
+}
